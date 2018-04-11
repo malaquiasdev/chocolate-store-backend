9a9cfc6fae7aed3c20f404c79d2fe9bd1b50ebb1
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2018-04-10 22:16:55 -0300

    initial commit

---
+++ src/main/java/com/github/mmalaquiasdev/chocolatestore/ChocolateStoreApplication.java
@@ -0,0 +1,12 @@
+package com.github.mmalaquiasdev.chocolatestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChocolateStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChocolateStoreApplication.class, args);
+	}
+}
